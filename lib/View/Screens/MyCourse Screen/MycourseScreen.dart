4e4c74d6d9d2d75b96af6595d25859eb0721c38f
AhmmedThanveer <thanveerahmmed4@gmail.com>
2024-09-12 06:18:36 -0700

    course screen add stacked image container and  changes

--- lib/View/Screens/MyCourse Screen/MycourseScreen.dart
+++ lib/View/Screens/MyCourse Screen/MycourseScreen.dart
@@ -12,6 +12,7 @@ import 'package:troganapp/View%20Model/Provider/ApiService.dart';
 import 'package:troganapp/View/Screens/Course%20Listing%20Screen/courselistingscreen.dart';
 // import 'package:troganapp/View/Screens/Home%20Screen/Widgets/Bottombar.dart';
 import 'package:troganapp/View/Screens/MyCourse%20Screen/Widgets/Courseround.dart';
+import 'package:troganapp/View/Screens/MyCourse%20Screen/Widgets/StackedContainer.dart';
 import 'package:troganapp/View/Screens/MyCourse%20Screen/Widgets/TestseriesItem.dart';
 import 'package:troganapp/View/Screens/MyCourse%20Screen/Widgets/appbarhome.dart';
 import 'package:troganapp/View/Screens/MyCourse%20Screen/Widgets/griditem.dart';
@@ -157,78 +158,7 @@ class _HomescreenState extends State<Mycoursescreen> {
                       },
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 15),
-                    child: Stack(
-                      clipBehavior: Clip
-                          .none, // Allows the child to be outside of the box
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(left: 20, right: 20),
-                          child: Container(
-                            width: double.infinity,
-                            height: 150,
-                            padding: EdgeInsets.only(
-                                top: 40,
-                                left: 20,
-                                right: 20,
-                                bottom: 20), // Adjusted padding
-                            decoration: BoxDecoration(
-                              color: Color.fromRGBO(246, 234, 182,
-                                  0.5), // Set the RGBA color here
-                              borderRadius: BorderRadius.circular(12),
-                              // boxShadow: [
-                              //   BoxShadow(
-                              //     color: Colors.black26,
-                              //     blurRadius: 8,
-                              //     offset: Offset(0, 2),
-                              //   ),
-                              // ],
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Text(
-                                  'Practice With Previous Year\nQuestion Papers',
-                                  style: GoogleFonts.inter(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                ),
-                                SizedBox(height: 10), // Space for the image
-                              ],
-                            ),
-                          ),
-                        ),
-                        Positioned(
-                            top:
-                                -30, // Position where part of the image is outside the container
-                            right: 30, // Left margin from the container
-                            child:
-                                Image.asset('assets/images/pngtree-boy.png')),
-                        Positioned(
-                          bottom:
-                              -20, // Position the button half outside the top of the container
-                          left: 0,
-                          right: 0,
-                          child: Center(
-                            // Center the button horizontally
-                            child: CircleAvatar(
-                              radius: 25,
-                              backgroundColor: Colors.white,
-                              child: CircleAvatar(
-                                radius: 20,
-                                backgroundColor: Colors.deepPurple,
-                                child: Icon(Icons.arrow_forward,
-                                    color: Colors.white),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
+                  StackedImageContainerWidget(),
                   Padding(
                     padding: const EdgeInsets.only(
                         left: 15, right: 15, top: 20, bottom: 10),
--- lib/View/Screens/MyCourse Screen/Widgets/TestseriesItem.dart
+++ lib/View/Screens/MyCourse Screen/Widgets/TestseriesItem.dart
@@ -10,7 +10,7 @@ class TestseriesGriditem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 180,
+      height: 160,
       child: ListView.builder(
         shrinkWrap: true,
 
